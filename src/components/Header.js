08f10f8063d677130946e1a0719867b749aaa561
Ramnath K <ramnath2609@gmail.com>
2022-07-25 19:32:07 +0530

    changed default font

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ export function Header() {
   return (
     <div className="d-flex justify-content-between align-items-center border-success m-2 border-bottom">
       <div className="d-flex align-items-center m-2">
-        <div className='h3 m-0 p-2' style={{ color: '#30336b' }}>Webine3</div>
+        <div className='h3 m-0 p-2 fw-bold' style={{ color: '#30336b' }}>Webine3</div>
         <button className="btn btn-light">Supplier</button>
       </div>
       <div className='d-flex align-items-center'>
